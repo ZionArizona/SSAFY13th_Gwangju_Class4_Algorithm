52f0211b8072bc3ceb8f4987a247968dfa45ae05
ZionArizona <93242434+ZionArizona@users.noreply.github.com>
2025-01-17 22:33:50 +0900

    Update 2751_수 정렬하기 2.java

--- 권시온/정렬/2751_수 정렬하기 2.java
+++ 권시온/정렬/2751_수 정렬하기 2.java
@@ -1,3 +1,6 @@
+// 단순 Arrays.sort() 는 시간 복잡도가 최악의 경우 n^2(dual-pivot Quicksort 어쩌고 저쩌고)
+// 이기 때문에 시간 초과 발생( 평균 시간 복잡도 n*logn)
+// 다른 방법으로 Collections.sort() 라는 Timesort를 이용( 시간 복잡도 n ~ n*long). 단, Collections.sort()는 리스트를 사용.
 import java.util.*;
 import java.io.*;
 
