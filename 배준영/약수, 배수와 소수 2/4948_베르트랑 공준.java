c84cc28f2f10e80561bcf25b61dd4a9c0d210507
Junyeong Bae <101334646+JunYBae@users.noreply.github.com>
2025-01-25 13:48:20 +0900

    Create 4948_베르트랑 공준.java

---
+++ 배준영/약수, 배수와 소수 2/4948_베르트랑 공준.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+
+class Main {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int number = 0;
+		
+		while(true) {
+			
+			number = sc.nextInt();
+			
+			if(number == 0)
+				break;
+			
+			int count = 0;
+			
+			for (int n = number+1; n <= 2 * number; n++)
+			{
+				if(is_Prime(n))
+					count++;
+			}
+			
+			System.out.println(count);
+			
+		}	
+		
+	}
+	
+	
+	public static boolean is_Prime(int x) {
+		
+		boolean is_prime = true;
+		
+		if (x == 1)
+			return false;
+		
+		for (int i = 2; i <= x/2; i++)
+		{
+			if (x % i == 0) 
+			{
+				is_prime = false;
+				break;
+			}
+		}		
+			
+		return is_prime;
+	}
+	
+
+}
