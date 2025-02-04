03c9b209a663d545527595519a3a63f7d28813bb
Tery <44861398+crew8264@users.noreply.github.com>
2025-02-05 01:12:24 +0900

    Create b4_24723.java

---
+++ 김용빈/조합론/b4_24723.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+// 24723 : 녹색거탑
+public class b4_24723 {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		
+		System.out.println((int)(Math.pow(2, N)));
+	}
+}
