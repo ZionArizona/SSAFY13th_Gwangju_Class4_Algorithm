8b205f4cfc508f19f32f44c130c5e12ef933e09e
Tery <44861398+crew8264@users.noreply.github.com>
2025-02-07 00:02:37 +0900

    Create b1_1934.java

---
+++ 김용빈/약수, 배수와 소수/b1_1934.java
@@ -0,0 +1,33 @@
+package day02;
+
+import java.util.Scanner;
+
+public class b1_1934 {
+	public static int gcd(int A, int B) {
+		int R = 0;
+		
+		while(B != 0) {
+			R = A%B;
+			A = B;
+			B = R;
+		}
+		return A;
+	}
+	public static int lcm(int A, int B) {
+		return (A*B) / gcd(A, B);
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		sc.nextLine();
+		
+		for (int i = 0; i < T; i++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			System.out.println(lcm(A, B));
+		}
+	}
+}
