8b930d423827058aa7bb35f587346955fe61ae40
ZionArizona <93242434+ZionArizona@users.noreply.github.com>
2025-02-10 08:44:08 +0900

    Create 89799471_녹색거탑.java

---
+++ 권시온/조합론/89799471_녹색거탑.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        
+        int ans = (int)Math.pow(2,N);
+        
+        System.out.println(ans);
+        
+        
+    }
+}
